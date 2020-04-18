6db20f43c6387adb3b739b9452c0f32047cb66ca
Calvin Jian <cjian1@binghamton.edu>
2020-04-18 19:20:47 -0400

    adjacency matrix

--- primMatrix.cpp
+++ primMatrix.cpp
@@ -1,13 +1,23 @@
-int smallestVertex(int key[], int mst[]) {
+#include <bits/stdc++.h>
+using namespace std;
+#define V 5
+
+int smallestVertex(int key[], bool mst[]) {
   int smallest = INT_MAX;
   int index;
-  for (int i=0; i<key.size; i++) {
+  for (int i=0; i<V; i++) {
     if (mst[i] == false && key[i] < smallest) {
       smallest = key[i];
       index = i;
     }
   }
-  return i;
+  return index;
+}
+
+void printMST(int key[], int parent[]) {
+  for (int i=1; i<V; i++) {
+    cout << "Parent: " << parent[i] << " Vertex: " << i << ", Distance: " << key[i] << endl;
+  }
 }
 
 void primMatrixWithoutPriorityQ(int Graph[V][V]) {
@@ -28,8 +38,16 @@ void primMatrixWithoutPriorityQ(int Graph[V][V]) {
       }
     }
   }
+  printMST(key, parent);
 }
 
 int main() {
-  primMatrixWithoutPriorityQ();
+  int Graph[5][5] = {
+    {0, 3, 4, 1, 5},
+    {3, 0, 2, 3, 2},
+    {4, 2, 0, 7, 8},
+    {1, 3, 7, 0, 0},
+    {5, 2, 8, 0, 0}
+  };
+  primMatrixWithoutPriorityQ(Graph);
 }
